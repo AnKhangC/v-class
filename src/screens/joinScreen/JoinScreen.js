57b0b7105540560d00d219f0514aacec92c0875d
chaimaef <chaimaefahmi@outlook.com>
2020-10-17 19:44:23 -0400

    Added join meeting form

--- src/screens/joinScreen/JoinScreen.js
+++ src/screens/joinScreen/JoinScreen.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from '@material-ui/core';
-import styles from './JoinScreen.module.css'
+import styles from './JoinScreen.module.css';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import Image from 'react-bootstrap/Image';
 
 function JoinScreen() {
   return (
     <>
-      <div>
-        <Button variant="contained" color="primary" style={{minWidth: '50%', maxWidth: '50%', minHeight: '100px', maxHeight: '100px'}}>
-          <Link to="/seating" className={styles.linkClass}>Join Meeting</Link>
-        </Button>
+      <div className={styles.formContainer}>
+        <div className={styles.logoContainer}>
+          <Image className={styles.logo} src={require('../../Images/logo.png')} />
+        </div>
+
+        <div className={styles.fieldsContainer} >
+          <Box m={1}>
+            <TextField className={styles.formFields} id="filled-basic" label="Username" variant="filled" />
+          </Box>
+          <Box m={1}>
+            <TextField className={styles.formFields} id="filled-basic" label="Meeting ID" variant="filled" />
+          </Box>
+        </div>
+
+        <div className={styles.buttonContainer}>
+          <Button variant="contained" color="primary" >
+            <Link to="/seating" className={styles.linkClass}>Join Meeting</Link>
+          </Button>
+        </div>
       </div>
     </>
   )
