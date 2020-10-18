a9bdc59d25c52da9fd7f2f0a334123297a1e1d49
AnKhangC <an.k.chau@mail.mcgill.ca>
2020-10-17 21:48:57 -0400

    seating map implemented but missing some styling

---
+++ src/screens/seatingScreen/Seat/Seat.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import styles from './Seat.module.css'
+
+function Seat(props) {
+
+  function onClick() {
+    let index = props.row.findIndex(x => x.id === props.seat.id); 
+
+    if (props.row[index].occupied === true) {
+      return;
+    } else {
+      var newRow = props.row.slice();
+      for (var i = 0; i < newRow.length; i++) {
+        if (newRow[i].selected === true) {
+          newRow[i].selected = false;
+        }
+      }
+      props.row[index].selected = true;
+      props.setRow(newRow);
+    }
+  }
+
+  return (
+    <>
+      <div className={styles.seat}>
+        <Button
+          onClick={onClick}
+          className={styles.circle}
+          variant="contained"
+          style={{
+            borderRadius: 25,
+            margin: 10,
+            minWidth: '50px',
+            maxWidth: '50px',
+            minHeight: '50px',
+            maxHeight: '50px',
+            backgroundColor: ((props.seat.occupied === true) ? "grey" : ((props.seat.selected === true) ? "blue" : "white"))
+          }}
+        />
+        <p className={styles.name}>{props.seat.selected === true && "My Seat"}</p>
+        <p className={styles.name}>{props.seat.selected === false && props.seat.name}</p>
+      </div>
+    </>
+  )
+}
+
+export default Seat
+
