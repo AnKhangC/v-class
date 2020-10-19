be9844cfc2c3d6d24f8cb32fc678280e32f69ba2
RogerZhang <roger.zhang@publicissapient.com>
2020-10-19 12:02:18 -0400

    Added random answers from imaginary users

--- src/screens/zoomScreen/ZoomScreen.js
+++ src/screens/zoomScreen/ZoomScreen.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import styles from './ZoomScreen.module.css'
 import { connect } from "react-redux"
+import { Button } from '@material-ui/core';
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 class ZoomScreen extends React.Component {
 
@@ -8,19 +13,26 @@ class ZoomScreen extends React.Component {
     super(props);
     this.state = {
       names: ["Mike", "Alexis", "Antoine", "Sam", "Farouk"], 
-      messageHistory: ["Alexis: Hello", "Antoine: Heyy"],
-      message: ''
+      messageHistory: [],
+      message: '',
+      helloMessageBank: ["Hello", "Heyyy", "Hey what's up", "Hey guyss", "Hii"],
+      questionAnswerMessageBank: ["Idk", "I don't really know", "I think so", "Yeahh", "Probably", "I wasn't paying attention lol"],
+      counter: 0
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.updateMesssageHistory = this.updateMesssageHistory.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.getRandomName = this.getRandomName.bind(this);
+    this.getRandomHello = this.getRandomHello.bind(this);
   }
 
+  //When send button is clicked
   handleChange(event) {
     this.setState({message: event.target.value});
   }
 
+  //When enter key is pressed
   handleKeyDown(e){
     console.log(this.props.neighbours)
     if (e.key === 'Enter') {
@@ -28,11 +40,45 @@ class ZoomScreen extends React.Component {
     }
   }
 
+  //Update messageHistory with message from sender
   updateMesssageHistory(){
     if(this.state.message !== ''){
       this.setState({messageHistory: this.state.messageHistory.concat('You: ' + this.state.message)});
       this.setState({message: ''});
     }
+
+    //Send random message from imaginary neighbors
+    setTimeout(() => {
+      if(this.state.counter === 0){
+        this.setState({messageHistory: this.state.messageHistory.concat(this.getRandomName() + ": " + this.getRandomHello())});
+        setTimeout(() => {
+          this.setState({messageHistory: this.state.messageHistory.concat(this.getRandomName() + ": " + this.getRandomHello())});
+        }, 500);
+        this.setState({counter: this.state.counter + 1});
+      } else {
+        this.setState({messageHistory: this.state.messageHistory.concat(this.getRandomName() + ": " + this.getRandomQuestionAnswer())});
+        setTimeout(() => {
+          this.setState({messageHistory: this.state.messageHistory.concat(this.getRandomName() + ": " + this.getRandomQuestionAnswer())});
+        }, 1000);
+      }
+    }, 1000);
+
+    
+  }
+
+  //Get random name from name bank
+  getRandomName(){
+    return this.state.names[getRandomInt(this.state.names.length)]
+  }
+
+  //Get random greeting from greetings bank
+  getRandomHello(){
+    return this.state.helloMessageBank[getRandomInt(this.state.helloMessageBank.length)]
+  }
+
+  //Get random answer from answer bank
+  getRandomQuestionAnswer(){
+    return this.state.questionAnswerMessageBank[getRandomInt(this.state.questionAnswerMessageBank.length)]
   }
 
   render(){
