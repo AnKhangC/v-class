39216f178dbb7f73529c6172bb12b58203b07df3
RogerZhang <roger.zhang@publicissapient.com>
2020-11-15 22:25:26 -0500

    implement an all chat

--- src/screens/zoomScreen/ZoomScreen.js
+++ src/screens/zoomScreen/ZoomScreen.js
@@ -10,101 +10,153 @@ function getRandomInt(max) {
 
 class ZoomScreen extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      names: ["Jessica", "Martin", "Naissa", "Jack", "Chaimae"], 
-      messageHistory: [],
-      message: '',
+      names: ["Jessica", "Martin", "Naissa", "Jack", "Chaimae"],
+      neighbourMessageHistory: [],
+      neighbourMessage: '',
+      allMessageHistory: [],
+      allMessage: '',
       helloMessageBank: ["Hello", "Heyyy", "Hey what's up", "Hey guyss", "Hii"],
       questionAnswerMessageBank: ["Idk", "I don't really know", "I think so", "Yeahh", "Probably", "I wasn't paying attention lol"],
       counter: 0
     }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.updateMesssageHistory = this.updateMesssageHistory.bind(this);
+    this.handleChangeAll = this.handleChangeAll.bind(this);
+    this.updateAllMesssageHistory = this.updateAllMesssageHistory.bind(this);
+    this.handleChangeNeighbour = this.handleChangeNeighbour.bind(this);
+    this.updateNeighbourMesssageHistory = this.updateNeighbourMesssageHistory.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.getRandomName = this.getRandomName.bind(this);
     this.getRandomHello = this.getRandomHello.bind(this);
     this.isAlone = this.isAlone.bind(this);
 
-    if(this.isAlone()){
+    if (this.isAlone()) {
       this.state.names = [];
     }
   }
 
-  //When send button is clicked
-  handleChange(event) {
-    this.setState({message: event.target.value});
+  //When send button is clicked for all chat
+  handleChangeAll(event) {
+    this.setState({ allMessage: event.target.value });
+  }
+
+  //When send button is clicked for neighbour chat
+  handleChangeNeighbour(event) {
+    this.setState({ neighbourMessage: event.target.value });
   }
 
   //When enter key is pressed
-  handleKeyDown(e){
-    console.log(this.props.neighbours)
+  handleKeyDown(e) {
     if (e.key === 'Enter') {
-      this.updateMesssageHistory();
+      this.updateAllMesssageHistory();
+      this.updateNeighbourMesssageHistory();
     }
   }
 
-  //Update messageHistory with message from sender
-  updateMesssageHistory(){
-    if(this.state.message !== ''){
-      this.setState({messageHistory: this.state.messageHistory.concat('You: ' + this.state.message)});
-      this.setState({message: ''});
+  //Update messageHistory for all chat with message from sender
+  updateAllMesssageHistory() {
+    if (this.state.allMessage !== '') {
+      this.setState({ allMessageHistory: this.state.allMessageHistory.concat('You: ' + this.state.allMessage) });
+      this.setState({ allMessage: '' });
     }
 
     //Send random message from imaginary neighbors
-    if(this.state.names.length !== 0){
-    setTimeout(() => {
-      if(this.state.counter === 0){
-        this.setState({messageHistory: this.state.messageHistory.concat(this.getRandomName() + ": " + this.getRandomHello())});
-        setTimeout(() => {
-          this.setState({messageHistory: this.state.messageHistory.concat(this.getRandomName() + ": " + this.getRandomHello())});
-        }, 500);
-        this.setState({counter: this.state.counter + 1});
-      } else {
-        this.setState({messageHistory: this.state.messageHistory.concat(this.getRandomName() + ": " + this.getRandomQuestionAnswer())});
-        setTimeout(() => {
-          this.setState({messageHistory: this.state.messageHistory.concat(this.getRandomName() + ": " + this.getRandomQuestionAnswer())});
-        }, 1000);
-      }
-    }, 1000);
+    if (this.state.names.length !== 0 && this.state.allMessage !== '') {
+      setTimeout(() => {
+        if (this.state.counter === 0) {
+          this.setState({ allMessageHistory: this.state.allMessageHistory.concat(this.getRandomName() + ": " + this.getRandomHello()) });
+          setTimeout(() => {
+            this.setState({ allMessageHistory: this.state.allMessageHistory.concat(this.getRandomName() + ": " + this.getRandomHello()) });
+          }, 500);
+          this.setState({ counter: this.state.counter + 1 });
+        } else {
+          this.setState({ allMessageHistory: this.state.allMessageHistory.concat(this.getRandomName() + ": " + this.getRandomQuestionAnswer()) });
+          setTimeout(() => {
+            this.setState({ allMessageHistory: this.state.allMessageHistory.concat(this.getRandomName() + ": " + this.getRandomQuestionAnswer()) });
+          }, 1000);
+        }
+      }, 1000);
+    }
+  }
+
+  //Update messageHistory for neighbour chat with message from sender
+  updateNeighbourMesssageHistory() {
+    if (this.state.neighbourMessage !== '') {
+      this.setState({ neighbourMessageHistory: this.state.neighbourMessageHistory.concat('You: ' + this.state.neighbourMessage) });
+      this.setState({ neighbourMessage: '' });
+    }
+
+    //Send random message from imaginary neighbors
+    if (this.state.names.length !== 0 && this.state.neighbourMessage !== '') {
+      setTimeout(() => {
+        if (this.state.counter === 0) {
+          this.setState({ neighbourMessageHistory: this.state.neighbourMessageHistory.concat(this.getRandomName() + ": " + this.getRandomHello()) });
+          setTimeout(() => {
+            this.setState({ neighbourMessageHistory: this.state.neighbourMessageHistory.concat(this.getRandomName() + ": " + this.getRandomHello()) });
+          }, 500);
+          this.setState({ counter: this.state.counter + 1 });
+        } else {
+          this.setState({ neighbourMessageHistory: this.state.neighbourMessageHistory.concat(this.getRandomName() + ": " + this.getRandomQuestionAnswer()) });
+          setTimeout(() => {
+            this.setState({ neighbourMessageHistory: this.state.neighbourMessageHistory.concat(this.getRandomName() + ": " + this.getRandomQuestionAnswer()) });
+          }, 1000);
+        }
+      }, 1000);
     }
   }
 
   //Get random name from name bank
-  getRandomName(){
+  getRandomName() {
     return this.state.names[getRandomInt(this.state.names.length)]
   }
 
   //Get random greeting from greetings bank
-  getRandomHello(){
+  getRandomHello() {
     return this.state.helloMessageBank[getRandomInt(this.state.helloMessageBank.length)]
   }
 
   //Get random answer from answer bank
-  getRandomQuestionAnswer(){
+  getRandomQuestionAnswer() {
     return this.state.questionAnswerMessageBank[getRandomInt(this.state.questionAnswerMessageBank.length)]
   }
 
-  isAlone(){    
+  isAlone() {
     var index = this.props.neighbours.findIndex(x => x.selected === true)
-    if(index >= 0){
+    if (index >= 0) {
       return false;
     } else {
       return true;
     }
   }
 
-  render(){
-    return(
-      <div className = {styles.global}>
-        <div className = {styles.outerFrame}>
-          <div className = {styles.zoom}>
-            <img className = {styles.zoomImage} src={require('./NoParticipation1.jpg')} />
+  render() {
+    return (
+      <div className={styles.global}>
+        <div className={styles.outerFrame}>
+          <div className={styles.zoom}>
+            <img className={styles.zoomImage} src={require('./NoParticipation1.jpg')} />
           </div>
-          <div className = {styles.chatBox}>
-            <div className = {styles.participantList}>
+          <div className={styles.allChatBox}>
+            <div className={styles.allChatTitle}>
+              <h1>All Chat</h1>
+            </div>
+            <div className={styles.allChat}>
+              <ul>
+                {this.state.allMessageHistory.map((value, index) => {
+                  return <li key={index}>{value}</li>
+                })}
+              </ul>
+            </div>
+            <div className={styles.message}>
+              <input type="text" value={this.state.allMessage} placeholder="Message..." onChange={this.handleChangeAll}
+                onKeyDown={this.handleKeyDown} />
+              <Button type="button" onClick={this.updateAllMesssageHistory}>Send</Button>
+            </div>
+          </div>
+          <div className={styles.chatBox}>
+            <div className={styles.participantList}>
               <h1>Neighbors</h1>
               <ul>
                 {this.state.names.map((value, index) => {
@@ -112,22 +164,22 @@ class ZoomScreen extends React.Component {
                 })}
               </ul>
             </div>
-            <div className = {styles.liveChat}>
+            <div className={styles.neighbourChat}>
               <ul>
-              {this.state.messageHistory.map((value, index) => {
+                {this.state.neighbourMessageHistory.map((value, index) => {
                   return <li key={index}>{value}</li>
                 })}
               </ul>
             </div>
-            <div className = {styles.message}>
-              <input type = "text" value={this.state.message} placeholder = "Message..." onChange = {this.handleChange} 
-              onKeyDown = {this.handleKeyDown}/>
-              <Button type = "button" onClick = {this.updateMesssageHistory}>Send</Button>
+            <div className={styles.message}>
+              <input type="text" value={this.state.neighbourMessage} placeholder="Message..." onChange={this.handleChangeNeighbour}
+                onKeyDown={this.handleKeyDown} />
+              <Button type="button" onClick={this.updateNeighbourMesssageHistory}>Send</Button>
             </div>
           </div>
         </div>
-        <div className = {styles.leaveButton}>
-          <Button type = "button" variant="contained">
+        <div className={styles.leaveButton}>
+          <Button type="button" variant="contained">
             <Link to="/" className={styles.linkClass}>Leave</Link>
           </Button>
         </div>
